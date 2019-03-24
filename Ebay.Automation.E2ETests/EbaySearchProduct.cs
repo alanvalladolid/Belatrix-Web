b6e21e771cfdb57b165c6c9a6a7e97e6a02605b0
Jorge Yanac <jyanac@aptos.com>
2019-03-24 18:50:41 -0500

    first commit

---
+++ Ebay.Automation.E2ETests/EbaySearchProduct.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebay.Automation.E2ETests
+{
+   [TestClass]
+    public class EbaySearchProductTests
+    {
+        private IWebDriver driver;
+        private string url;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            url = "http://www.ebay.com";
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void PrintNumberOfSearchResults()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(By.Id("gh-ac")).SendKeys("shoes");
+            driver.FindElement(By.Id("gh-btn")).Click();
+            driver.FindElement(By.XPath("//*[@id='w3-w0-w2-w2-w6']/a")).Click();
+            driver.FindElement(By.XPath("//*[@id='w3-w0-w2-multiselect[4]']/a")).Click();
+            var searchResultNumber = driver.FindElement(By.XPath("//*[@id='w4']/div[2]/div/div[1]/h1")).Text;
+            Console.WriteLine(searchResultNumber);
+            Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+
+
+        }
+    }
+
+    
+}
---
+++ Ebay.Automation.E2ETests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ebay.Automation.E2ETests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ebay.Automation.E2ETests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1db409e-1561-4b4e-a4e0-74a88df25936")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
